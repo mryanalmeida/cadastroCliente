622511c24e2943d26da52cd6c070a05834abe8ca
Marcos Almeida <tubaraogalatico2509@gmail.com>
2024-10-18 17:31:37 -0300

    cadastrocliente

--- script.js
+++ script.js
@@ -25,30 +25,64 @@ function buscarEndereco() {
                     const telefoneInput = document.getElementById('telefone');
                     telefoneInput.value = `(${ddd}) `; // Preenche com o DDD inicial
                 } else {
-                    alert('CEP não encontrado.');
+                    mostrarMensagemErro('CEP não encontrado.');
                 }
             })
             .catch(error => console.error('Erro ao buscar o endereço:', error));
     } else {
-        alert('CEP inválido.');
+        mostrarMensagemErro('CEP inválido.');
     }
 }
 
 // Função de busca de DDD com base na cidade
 function buscarDDD(cidade) {
     const ddds = {
-        "São Paulo": "11",
+        "Acre": "68",
+        "Alagoas": "82",
+        "Amapá": "96",
+        "Amazonas": "92",
+        "Bahia": "71",
+        "Ceará": "85",
+        "Distrito Federal": "61",
+        "Espírito Santo": "27",
+        "Goiás": "62",
+        "Maranhão": "98",
+        "Mato Grosso": "65",
+        "Mato Grosso do Sul": "67",
+        "Minas Gerais": "31",
+        "Pará": "91",
+        "Paraíba": "83",
+        "Paraná": "41",
+        "Pernambuco": "81",
+        "Piauí": "86",
         "Rio de Janeiro": "21",
+        "Rio Grande do Norte": "84",
+        "Rio Grande do Sul": "51",
+        "Rondônia": "69",
+        "Roraima": "95",
+        "Santa Catarina": "48",
+        "São Paulo": "11",
+        "Sergipe": "79",
+        "Tocantins": "63",
+        "São Luís": "98",
+        "Fortaleza": "85",
+        "Salvador": "71",
         "Belo Horizonte": "31",
         "Brasília": "61",
-        "Salvador": "71",
-        "Fortaleza": "85",
         "Curitiba": "41",
         "Manaus": "92",
         "Porto Alegre": "51",
         "Recife": "81",
-        // Adicionar mais cidades e seus respectivos DDDs conforme necessário
+        // Adicione mais cidades conforme necessário
     };
 
     return ddds[cidade] || ''; // Retorna o DDD ou uma string vazia se a cidade não for encontrada
+}
+
+
+// Função para mostrar mensagens de erro de forma amigável
+function mostrarMensagemErro(mensagem) {
+    const mensagemErroDiv = document.getElementById('mensagem-erro');
+    mensagemErroDiv.textContent = mensagem;
+    mensagemErroDiv.style.display = 'block'; // Mostra a mensagem
 }
